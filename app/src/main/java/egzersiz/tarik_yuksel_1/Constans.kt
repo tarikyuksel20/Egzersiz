c33ef66e9d03533ca1ee3d40472d31b7307e5157
tarikyuksel20 <67550819+tarikyuksel20@users.noreply.github.com>
2023-01-22 20:39:57 +0300

    finished project

---
+++ app/src/main/java/egzersiz/tarik_yuksel_1/Constans.kt
@@ -0,0 +1,77 @@
+package egzersiz.tarik_yuksel_1
+
+object Constans {
+    fun defaultExerciseList():ArrayList<ExerciseModel>{
+        val exerciseList = ArrayList<ExerciseModel>()
+        val jumpingjacks =  ExerciseModel(
+            1,
+            "jumping Jacks",
+            R.drawable.jumpingjacks,
+            false,
+            false
+        )
+        exerciseList.add(jumpingjacks)
+
+        val wallSit = ExerciseModel(2, "Wall Sit", R.drawable.wall_sit, false, false)
+        exerciseList.add(wallSit)
+
+        val pushUp = ExerciseModel(3, "Push Up", R.drawable.push_up, false, false)
+        exerciseList.add(pushUp)
+
+        val abdominalCrunch =
+            ExerciseModel(4, "Abdominal Crunch", R.drawable.abdominal_crunch, false, false)
+        exerciseList.add(abdominalCrunch)
+
+        val stepUpOnChair =
+            ExerciseModel(
+                5,
+                "Step-Up onto Chair",
+                R.drawable.step_up_onto_chair,
+                false,
+                false
+            )
+        exerciseList.add(stepUpOnChair)
+
+        val squat = ExerciseModel(6, "Squat", R.drawable.squat, false, false)
+        exerciseList.add(squat)
+
+        val tricepDipOnChair =
+            ExerciseModel(
+                7,
+                "Tricep Dip On Chair",
+                R.drawable.triceps_dip_on_chair,
+                false,
+                false
+            )
+        exerciseList.add(tricepDipOnChair)
+
+        val plank = ExerciseModel(8, "Plank", R.drawable.plank, false, false)
+        exerciseList.add(plank)
+
+        val highKneesRunningInPlace =
+            ExerciseModel(
+                9, "High Knees Running In Place",
+                R.drawable.high_kness,
+                false,
+                false
+            )
+        exerciseList.add(highKneesRunningInPlace)
+
+        val lunges = ExerciseModel(10, "Lunges", R.drawable.lunge, false, false)
+        exerciseList.add(lunges)
+
+        val pushupAndRotation =
+            ExerciseModel(
+                11,
+                "Push up and Rotation",
+                R.drawable.push_up,
+                false,
+                false
+            )
+        exerciseList.add(pushupAndRotation)
+
+        val sidePlank = ExerciseModel(12, "Side Plank", R.drawable.side_plank, false, false)
+        exerciseList.add(sidePlank)
+        return exerciseList
+    }
+}
